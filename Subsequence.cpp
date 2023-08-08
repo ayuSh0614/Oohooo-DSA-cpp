485f0c45417b84b8722e94124eb51f50af06fa98
ayuSh0614 <ayusharma051@gmail.com>
2023-08-08 15:18:52 +0530

    this

---
+++ Subsequence.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Time Complexity = O((2^n) * n)
+// Space Complexity = O(n)
+
+void printF(int ind, vector<int>& ds, int arr[], int n)
+{
+    if (ind == n)
+    {
+        for(auto it : ds)
+        {
+            cout<<it<<" ";
+        }
+        if (ds.size() == 0)
+        {
+            cout<<"{}";
+        }
+        
+        cout<<endl;
+        return;
+    }
+    // not pick, or not take condition, thiselement is not added to you subsequence
+    printF(ind+1, ds, arr, n);
+
+    
+    // take or pick the particular index into the subsequence
+    ds.push_back(arr[ind]);
+    printF(ind+1, ds, arr, n);
+    ds.pop_back();
+
+
+    
+}
+
+int main()
+{
+    int arr[] = {3,1,2};
+    int n = 3;
+    vector<int> ds;
+    printF(0, ds, arr, n);
+    
+    return 0;
+}
