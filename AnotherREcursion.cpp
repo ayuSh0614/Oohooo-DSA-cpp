498f46f2a230661bbdea9ed48db65c3730a7db9a
ayuSh0614 <ayusharma051@gmail.com>
2023-08-08 10:15:56 +0530

    this

--- AnotherREcursion.cpp
+++ AnotherREcursion.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
 using namespace std;
 
-void f(int i, int arr[], int n)
+// void f(int i, int arr[], int n)
+// {
+//     if (i >= n/2)
+//     {
+//         return;
+//     }
+//     swap(arr[i], arr[n-i-1]);
+//     f(i+1, arr, n);
+    
+// }
+
+bool CheckPalindrome(int i, string &s)
 {
-    if (i >= n/2)
+    if (i >= s.size()/2)
     {
-        return;
+        return true;
     }
-    swap(arr[i], arr[n-i-1]);
-    f(i+1, arr, n);
+    if (s[i] != s[s.size()-i-1])
+    {
+        return false;
+    }
+    return CheckPalindrome(i+1, s);
+    
     
 }
 
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin>>arr[i];
-    }
-    f(0, arr, n);
-    for (int i = 0; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // f(0, arr, n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+    string s = "madsm";
+    cout<<CheckPalindrome(0,s);
     
     
     return 0;
