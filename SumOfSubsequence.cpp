2215742a5e35d2b7b6736c7adda4966c81e552e9
ayuSh0614 <ayusharma051@gmail.com>
2023-08-09 12:13:59 +0530

    this

--- SumOfSubsequence.cpp
+++ SumOfSubsequence.cpp
@@ -29,9 +29,9 @@ void printS1(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
 }
 
 
-// To print any subsequence whose sum is sum
+// To print any subsequence whose sum is sum but this is not the optimised way
 bool flag = false;
-void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+void printS2(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
 {
     if (ind == n)
     {
@@ -49,13 +49,68 @@ void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
     }
     ds.push_back(arr[ind]);
     s += arr[ind];
-    printS(ind + 1, ds, s, sum, arr, n);
+    printS2(ind + 1, ds, s, sum, arr, n);
     s-= arr[ind];
     ds.pop_back();
 
     // not pick
-    printS(ind+1, ds, s,sum, arr, n);
+    printS2(ind+1, ds, s,sum, arr, n);
 }
+
+// Optimised way
+bool printS3(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
+        // condition Satisfied
+        if (s == sum)
+        {
+            for(auto it:ds)
+            {
+                cout<<it<<" ";
+            }
+            cout<<endl;
+            return true;
+        }
+        // condition not satisfied
+        else return false;
+        
+    }
+    ds.push_back(arr[ind]);
+    s += arr[ind];
+
+    if(printS3(ind + 1, ds, s, sum, arr, n) == true) return true;
+
+    s-= arr[ind];
+    ds.pop_back();
+
+    // not pick
+    if(printS3(ind+1, ds, s,sum, arr, n) == true) return true;
+}
+
+// Counting of subsequences
+int printS4(int ind, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
+        if (s == sum)
+        {
+            return 1;
+        }
+        else return 0;
+        
+    }
+    
+    s += arr[ind];
+    int l = printS4(ind + 1, s, sum, arr, n);
+    s-= arr[ind];
+    
+
+    // not pick
+    int r = printS4(ind+1, s,sum, arr, n);
+    return l + r;
+}
+
 
 int main()
 {
@@ -63,7 +118,8 @@ int main()
     int n = 3;
     int sum = 2;
     vector <int> ds;
-    printS(0, ds, 0, sum, arr, n);
+    // printS3(0, ds, 0, sum, arr, n);
+    cout<<printS4(0, 0, sum, arr, n);
     
     return 0;
 }
