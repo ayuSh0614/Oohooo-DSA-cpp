dc680bdc19205afbc11d6ddb3a9841437aa1c13c
ayuSh0614 <ayusharma051@gmail.com>
2023-08-09 11:21:36 +0530

    this

--- SumOfSubsequence.cpp
+++ SumOfSubsequence.cpp
@@ -2,12 +2,42 @@
 #include <vector>
 using namespace std;
 
-void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+// To print all the subsequence
+void printS1(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
 {
     if (ind == n)
     {
         if (s == sum)
         {
+            for(auto it:ds)
+            {
+                cout<<it<<" ";
+                cout<<endl;
+            }
+        }
+        return;
+        
+    }
+    ds.push_back(arr[ind]);
+    s += arr[ind];
+    printS1(ind + 1, ds, s, sum, arr, n);
+    s-= arr[ind];
+    ds.pop_back();
+
+    // not pick
+    printS1(ind+1, ds, s,sum, arr, n);
+}
+
+
+// To print any subsequence whose sum is sum
+bool flag = false;
+void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
+        if (s == sum && flag == false)
+        {
+            flag = true;
             for(auto it:ds)
             {
                 cout<<it<<" ";
